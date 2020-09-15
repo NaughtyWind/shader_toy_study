25a93830003ef4fae93422448af50c01b62aa10b
NaughtyWind <yunxiaomxd@163.com>
2020-09-15 16:41:54 +0800

    feat<L001>: 注释完毕1

--- L001/source.glsl
+++ L001/source.glsl
@@ -11,6 +11,7 @@ float length_n(vec2 v, float n) {
 }
 
 // polynomial smooth min (k = 0.1);
+// 考虑a，b过渡，给定k为控制点，a-b ∈ (-k, k)中做线性插值
 float smin(float a, float b, float k) {
     float h = clamp(0.5 + 0.5 * (b - a) / k, 0.0, 1.0);
     return mix(b, a, h) - k * h * (1.0 - h);
@@ -28,6 +29,7 @@ struct DistanceField {
     int id;
 };
 
+// 物体的表面的光照，环境光，高光，漫反射，材质粗糙系数
 struct Material {
     vec3 ambient;
     vec3 specular;
@@ -172,6 +174,7 @@ float ud_quad(vec3 p, vec3 a, vec3 b, vec3 c, vec3 d) {
 // operations
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// 结构体的并集运算
 DistanceField df_union(DistanceField d1, DistanceField d2) {
     if (d1.distance <= d2.distance)
         return d1;
@@ -179,18 +182,22 @@ DistanceField df_union(DistanceField d1, DistanceField d2) {
         return d2;
 }
 
+// 并集运算
 float op_union(float d1, float d2) {
     return min(d1, d2);
 }
 
+// 线性插值的并集
 float op_smooth_union(float d1, float d2, float r) {
     return smin(d1, d2, r);
 }
 
+// 差集运算
 float op_subtraction(float d1, float d2) {
     return max(d1, -d2);
 }
 
+// 交集运算
 float op_intersection(float d1, float d2) {
     return max(d1, d2);
 }
@@ -204,6 +211,7 @@ float op_intersection(float d1, float d2) {
 #define SCENE_GOLD 3
 #define SCENE_FLOOR 4
 
+// 根据不同物体设置不同的光照效果和材质
 void df_material(vec3 pos, int id, inout Material mtl) {
     if (id == SCENE_WHITE_PLASTIC) {
         mtl.ambient = vec3(0.0625, 0.0625, 0.0625);
@@ -243,15 +251,18 @@ void df_material(vec3 pos, int id, inout Material mtl) {
     }
 }
 
+// 计算光线与物体相交时的成像
 DistanceField df_scene(vec3 p) {
     // Base chassis.
-    // vec3(0.0, 0.25, 0.0) 球中心点，0.5球半径，sd_sphere 计算射线与球中心距离，然后计算距离与半径的差，可以得知射线与球是否相交
-    // vec3(1.0, 0.2, 0.6) 矩形的长宽高，0.05矩形的边角弧度，计算射线与矩形是否相交
+    // sd_sphere，计算圆球，vec3(0.0, 0.25, 0.0) 球中心点，0.5球半径，sd_sphere 计算射线与球中心距离，返回计算距离与半径的差，可以得知射线与球是否相交
+    // ud_round_box，计算矩形，vec3(1.0, 0.2, 0.6) 矩形的长宽高，0.05矩形的边角弧度，计算射线与矩形是否相交
+    // 集合的并集运算，对于过渡处做线性插值
     float chassis = op_smooth_union(
         sd_sphere(p - vec3(0.0, 0.25, 0.0), 0.5),
         ud_round_box(p, vec3(1.0, 0.2, 0.6), 0.05),
         0.1);
     // Cut out wheel arcs.
+    // 计算长方体轮子的四个孔，集合的差集运算
     chassis = op_subtraction(
         chassis,
         sd_capped_cylinder_z(p - vec3(-0.75, -0.2,  0.5), vec2(0.4, 0.2)));
@@ -265,6 +276,7 @@ DistanceField df_scene(vec3 p) {
         chassis,
         sd_capped_cylinder_z(p - vec3( 0.75, -0.2, -0.5), vec2(0.4, 0.2)));
 
+    // 计算每个轮子的位置和大小，并集运算
     float wheels = op_union(
         op_union(
             sd_torus82_z(p - vec3(-0.75, -0.2,  0.5), vec2(0.2, 0.1)),
@@ -273,30 +285,43 @@ DistanceField df_scene(vec3 p) {
             sd_torus82_z(p - vec3( 0.75, -0.2,  0.5), vec2(0.2, 0.1)),
             sd_torus82_z(p - vec3( 0.75, -0.2, -0.5), vec2(0.2, 0.1))));
 
+    // 定义运算完毕后的结构体，长方体和轮子
     DistanceField d1 = DistanceField(chassis, SCENE_WHITE_PLASTIC);
     DistanceField d2 = DistanceField(wheels, SCENE_GOLD);
 
+    // 球体，幕布
     DistanceField d3 = DistanceField(sd_sphere(p - vec3(0.75, 0.25, 0.0), 0.25), SCENE_RED_PLASTIC);
     DistanceField d4 = DistanceField(sd_plane(p, vec4(0.0, 1.0, 0.0, 0.5)), SCENE_FLOOR);
 
+    // 返回最终的物体并集运算
     return df_union(df_union(df_union(d1, d2), d3), d4);
 }
 
+/**
+ * 光线投射
+ * ray_pos，光线位置，光线方向
+ */
 int raymarch(vec3 ray_pos, vec3 ray_dir, out vec3 pos, out vec3 normal) {
     float t = 0.0;
+    // 投射最大长度
     for (int i = 0; i < 64; ++i) {
         // 对射线的长度做一个随变量t累加的过程
         vec3 pt = ray_pos + ray_dir * t;
+        // 获取显示的物体
         DistanceField df = df_scene(pt);
         float h = df.distance;
+        // 精度修正，在某个精度范围内判断是否获得成像，如果获得成像做修正并返回
         if (h < 0.001) {
             vec3 x_axis = 0.001 * vec3(1.0, 0.0, 0.0);
             vec3 y_axis = 0.001 * vec3(0.0, 1.0, 0.0);
             vec3 z_axis = 0.001 * vec3(0.0, 0.0, 1.0);
+            // 对物体做map修正
             normal.x = df_scene(pt + x_axis).distance - df_scene(pt - x_axis).distance;
             normal.y = df_scene(pt + y_axis).distance - df_scene(pt - y_axis).distance;
             normal.z = df_scene(pt + z_axis).distance - df_scene(pt - z_axis).distance;
+            // 单位化
             normal = normalize(normal);
+            // 记录射线位置
             pos = pt;
             return df.id;
         }
@@ -307,21 +332,32 @@ int raymarch(vec3 ray_pos, vec3 ray_dir, out vec3 pos, out vec3 normal) {
     return 0;
 }
 
+/**
+ * 光线投射阴影区域
+ * 光线位置，方向，最小距离，最大距离，阴影软硬程度系数（软硬程度，max —> 硬阴影，min -> 软阴影）
+ */
 float raymarch_shadow(vec3 ray_pos, vec3 ray_dir, float t_min, float t_max, float resolution) {
     float result = 1.0;
     float t = t_min;
+    // 计算次数
     for (int i = 0; i < 64; ++i)
     {
+        // 超过最大距离返回
         if (t >= t_max) break;
 
+        // 计算光线向量，postion + length * t
         vec3 pt = ray_pos + ray_dir * t;
+        // 获取相交的物体的相对位置长度
         float h = df_scene(pt).distance;
+        // 设置精度，完全挡住的相交区域直接返回0.0
         if (h < 0.001)
         {
             return 0.0;
         }
 
+        // 对原result与 (软硬系数 * 物体相对位置长度 / 当前最小距离)做比较，去最小值
         result = min(result, resolution * h / t);
+        // t做 一个累加处理，没有达到精度要求的，直接加上投射距离
         t += h;
     }
 
@@ -333,13 +369,13 @@ float raymarch_shadow(vec3 ray_pos, vec3 ray_dir, float t_min, float t_max, floa
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     fragColor = vec4(0.0, 0.0, 0.0, 1.0);
 
-    // 相机的原点，观察方向，上分量
+    // 相机的位置，观察方向，相机上分量
     float x_angle = PI / 6.0; // 30deg
     vec3 camera_pos = vec3(0.0, cos(-PI / 3.0), sin(-PI / 3.0));
     vec3 camera_dir = vec3(0.0, cos(2.0 * PI / 3.0), sin(2.0 * PI / 3.0));
     vec3 camera_up = vec3(0.0, cos(x_angle), sin(x_angle));
 
-    // 光线方向
+    // 光线位置，随时间变化
     vec3 light_pos = 2.0 * vec3(cos(2.0 * PI * iTime / 2.0), 1.0, sin(2.0 * PI * iTime / 2.0));
 
     // 画布划分为2 x 2的网格，做边界模糊采样处理，用于抗锯齿
@@ -358,31 +394,42 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
             vec3 ray_pos = camera_pos + uv.x * cross(camera_up, camera_dir) + uv.y * camera_up;
             // 射线方向
             vec3 ray_dir = camera_dir;
+            // 记录光线随时间变化的最大投射长度和成像物体
             vec3 pos, normal;
             float t;
 
+            // 对每块区域做光线投射，获取成像
             int id = raymarch(ray_pos, ray_dir, pos, normal);
+            // 判断光线与物体相交
             if (id > 0) {
                 Material mtl;
+                // 计算物体与射线的光照效果
                 df_material(pos, id, mtl);
 
+                // 入射光向量，相机观察向量，然后根据平行四边形法则计算出中间值向量
                 vec3 l = normalize(light_pos - pos);
                 vec3 v = normalize(camera_pos - pos);
                 vec3 h = normalize(v + l);
+                // 计算出中间值分别与入射光和观察点角度
                 float n_l = max(0.0, dot(normal, l));
                 float n_h = max(0.0, dot(normal, h));
 
+                // 阴影计算
+                // 光线位置，方向，最小距离，最大距离，分辨率
                 float shadow = raymarch_shadow(light_pos, -l, 0.0, 0.9 * distance(light_pos, pos), 32.0);
 
                 // Lambertian BRDF diffuse + Blinn-Phong BRDF specular
+                // 物理环境光扩散 + bling-phone光照模型高光处理
                 fragColor.rgb += mtl.ambient + shadow * n_l * (mtl.diffuse + (mtl.m + 2.0) / 8.0 * pow(n_h, mtl.m) * fresnel(mtl.specular, l, h));
             }
         }
     }
 
+    // 计算结果取一下经历n次采样后的平均值
     fragColor.rgb /= float(aa_count * aa_count);
 
     // Linear to gamma (sRGB) color space conversion (approximated).
+    // 将srgb（伽玛）以线性插值近似表示，柔和处理（滤镜效果？）
     float inv_gamma = 1.0 / 2.2;
     fragColor.rgb = pow(fragColor.rgb, vec3(inv_gamma, inv_gamma, inv_gamma));
 }
